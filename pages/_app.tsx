9e5e6df9fe930c72830647a41ba486f25d6749b8
Abhishek Sah <sah755146@gmail.com>
2022-01-23 23:33:35 +0530

    fixes FOUC aka large icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
-import '../node_modules/@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+
+config.autoAddCss = false
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
