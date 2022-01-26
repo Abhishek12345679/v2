edd46194932023258c15baedfe61410fddeb3610
Abhishek Sah <sah755146@gmail.com>
2022-01-26 20:11:57 +0530

    added plaiceholder

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const { withPlaiceholder } = require("@plaiceholder/next");
+
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+};
+module.exports = withPlaiceholder({
+  nextConfig,
+});
