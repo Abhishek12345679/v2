76636a5e15de4f08cb9f94fd31228d0856645a28
Abhishek Sah <sah755146@gmail.com>
2022-01-28 01:59:14 +0530

    tweaked avatar listening style

--- components/NowPlayingBubble.tsx
+++ components/NowPlayingBubble.tsx
@@ -4,7 +4,15 @@ const NowPlayingBubble = (props: NowPlayingProps) => {
     return (
         <div className="now-playing-bubble">
             <p className="white-font">
-                Listening to <br />{props.nowPlaying.title} <br /> by {props.nowPlaying.artist}  🎵
+                Listening to <br />
+                {props.nowPlaying.title?.length! > 40 ?
+                    props.nowPlaying.title?.substring(0, 20) :
+                    props.nowPlaying.title}...
+                <br />
+                by {props.nowPlaying.artist?.length! > 40 ?
+                    props.nowPlaying.artist?.substring(0, 20) :
+                    props.nowPlaying.artist}...
+                🎵
             </p>
         </div>
     );
