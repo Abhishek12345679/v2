e28aa9f52376b58012b07e8b5201d2111750fcac
Abhishek Sah <sah755146@gmail.com>
2022-02-11 18:04:29 +0530

    added the songurl anchor to the entire now playing now bubble

--- components/NowPlayingBubble.tsx
+++ components/NowPlayingBubble.tsx
@@ -9,48 +9,50 @@ const NowPlayingBubble = (props: NowPlayingProps) => {
     const nowPlayingBubble = document.getElementsByClassName('now-playing-bubble')
 
     return (
-        <div
-            style={{
-                background: !loading && data ? data.darkMuted : "#121212"
-            }}
-            className="now-playing-bubble"
-        >
-            {props.nowPlaying &&
-                <a href={props.nowPlaying.songUrl}>
-                    {props.nowPlaying.albumImageUrl &&
-                        <div className="rounded-rect">
-                            <Image
-                                src={props.nowPlaying.albumImageUrl}
-                                width={40}
-                                height={40}
-                                layout="fixed"
-                            />
-                        </div>
-                    }
-                </a>
-            }
-            {props.nowPlaying.isPlaying ?
-                <div>
-                    <p className="white-font bold padding-h-10">
-                        {props.nowPlaying.title?.length! > 40 ?
-                            props.nowPlaying.title?.substring(0, 20) :
-                            props.nowPlaying.title}
-                        {props.nowPlaying.title?.length! > 40 ? "..." : ""}
-                        <br />
-                        by {props.nowPlaying.artist?.length! > 40 ?
-                            props.nowPlaying.artist?.substring(0, 20) :
-                            props.nowPlaying.artist}
-                        {props.nowPlaying.title?.length! > 40 ? "..." : ""}
+        <a
+            target="_blank"
+            className="flex"
+            href={props.nowPlaying.songUrl}>
+            <div
+                style={{
+                    background: !loading && data ? data.darkMuted : "#121212"
+                }}
+                className="now-playing-bubble"
+            >
+
+                {props.nowPlaying.albumImageUrl &&
+                    <div className="rounded-rect">
+                        <Image
+                            src={props.nowPlaying.albumImageUrl}
+                            width={40}
+                            height={40}
+                            layout="fixed"
+                        />
+                    </div>
+                }
+                {props.nowPlaying.isPlaying ?
+                    <div>
+                        <p className="white-font bold padding-h-10">
+                            {props.nowPlaying.title?.length! > 40 ?
+                                props.nowPlaying.title?.substring(0, 20) :
+                                props.nowPlaying.title}
+                            {props.nowPlaying.title?.length! > 40 ? "..." : ""}
+                            <br />
+                            by {props.nowPlaying.artist?.length! > 40 ?
+                                props.nowPlaying.artist?.substring(0, 20) :
+                                props.nowPlaying.artist}
+                            {props.nowPlaying.title?.length! > 40 ? "..." : ""}
+                        </p>
+
+                    </div>
+                    :
+                    <p className="white-font font-size-15 padding-h-10">
+                        Not Playing 😦
                     </p>
+                }
 
-                </div>
-                :
-                <p className="white-font font-size-15 padding-h-10">
-                    Not Playing 😦
-                </p>
-            }
-
-        </div>
+            </div>
+        </a>
     );
 };
 
