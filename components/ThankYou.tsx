d2d4b255c5869c003cd7cbeb7c07946ba73ba9d4
Abhishek Sah <sah755146@gmail.com>
2022-02-11 18:00:29 +0530

    added an external link icon before the thank you comp to make it clearer

--- components/ThankYou.tsx
+++ components/ThankYou.tsx
@@ -1,8 +1,14 @@
+import { faExternalLink } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
 export const ThankYou = () => {
     return (
         <div className="margin-vert-20 white-font justify-center align-center text-center">
+            <FontAwesomeIcon
+                icon={faExternalLink}
+                size="sm"
+            />
             <Link href="/inspiration">
                 <a className="thankyou" title="Click this link to see the Websites inspired me.">
                     Thanks to Open source Creators and websites i drew inspiration from 🤗
