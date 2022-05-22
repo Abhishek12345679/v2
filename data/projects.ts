b752193200b4f71a84af242c0a9fe43016593092
Abhishek12345679 <sah755146@gmail.com>
2022-05-22 19:09:19 +0530

    baaaaaa

--- data/projects.ts
+++ data/projects.ts
@@ -1,7 +1,7 @@
 export const projects = [
   {
     image: "/images/projects/aushadhalay-new.png",
-    title: "aushadhalay",
+    title: "Aushadhalay",
     subtitle: "Scan and Buy Meds.",
     description:
       "Want your restock on your medicines? Just whip out your phone and snap a photo of the packaging and boom it shows you all the medicines matching the data from the packaging.\n\nWhy Aushadhalay instead of other apps\n1. Unlimited range of Medicinal Products 💊\n2. Intuitive 🐝\n3. One Scan away from your meds 🕵🏼‍♀️ 🕵🏼‍♂️",
@@ -18,7 +18,7 @@ export const projects = [
   },
   {
     image: "/images/projects/ordergator.png",
-    title: "ordergator",
+    title: "Ordergator",
     subtitle: "Aggregates all your shopping online.",
     description:
       "An app that lets you aggregate all your online shopping in a single app and lets you set reminders on them and much more.",
